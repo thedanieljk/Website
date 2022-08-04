60353727bd85c6cffb11483dad80c260cdca84eb
thedanieljk <thedanieljk@gmail.com>
2022-08-04 14:25:01 -0700

    description and about changes

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
-    email: 'janedoe.test@gmail.com',
+    email: 'thedanieljk@gmail.com',
     phone: '+915588776600',
-    address: 'Menlo Park, California, United States - 673822 ',
+    address: 'California, United States',
 
     sheetAPI: ''
 }
