36983406007a0a8d50379d377c88986437100339
thedanieljk <thedanieljk@gmail.com>
2022-08-04 14:35:49 -0700

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Daniel. I'm a software engineer."
-    description2: "I push buttons" 
+    description1: "My name's Daniel. I'm a software engineer.",
+    description2: "I push buttons",
     image: 2
 }
