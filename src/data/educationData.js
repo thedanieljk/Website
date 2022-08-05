b99fb2cb58f1d43c0ec038ad4f55b212945904ef
thedanieljk <thedanieljk@gmail.com>
2022-08-05 13:35:31 -0700

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -2,7 +2,7 @@ export const educationData = [
     {
         id: 1,
         institution: 'California Polytechnic State University',
-        course: 'Computer Science',
+        course: 'B.S Computer Science',
         startYear: '2014',
         endYear: '2020'
     }
