56e391828dd8d938355ffe28799a1402838ba082
thedanieljk <thedanieljk@gmail.com>
2022-08-04 19:07:48 -0700

    Update achievementData.js

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,20 +1,12 @@
 export const achievementData = {
-    bio : "Putting quantified achievements on a resume is great, but I just don’t work with hard numbers. I have no data to support the quality of my work. If you have lots of relevant experience, the heading statement of your resume will be a summary.",
+    description: "To expand the breadth of my knowledge, I got certifications in areas I felt my degree and experience did not cover.",
     achievements : [
         {
             id : 1,
-            title : 'Google Cloud certification',
-            details : 'Google Cloud certifications validate your expertise and showcase your ability to transform businesses with Google Cloud technology.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1573141597928-403fcee0e056?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80'
-        },
-        {
-            id : 2,
-            title : 'AWS Certification',
+            title : 'AWS Solutions Architect - Associate [SAA-C03]',
             details : 'Validate technical skills and cloud expertise to grow your career and business.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
+            date : 'September, 2022',
+            field : 'Cloud Architecture',
             image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
         }		
     ]
