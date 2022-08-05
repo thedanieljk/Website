9db9070ea5edb57d3fbc0723356224448a0e0a05
thedanieljk <thedanieljk@gmail.com>
2022-08-04 19:02:48 -0700

    remove phone #

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,5 @@
 export const contactsData = {
-    email: 'thedanieljk@gmail.com',
-    phone: '+915588776600',
-    address: 'California, United States',
+    email: 'thedanieljk@gmail.com'
 
     sheetAPI: ''
 }
