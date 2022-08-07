316ce25305aeeb1ea6933db6363193a7dbc521ae
thedanieljk <thedanieljk@gmail.com>
2022-08-07 13:06:15 -0700

    css changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import './Footer.css'
 import { ThemeContext } from '../../contexts/ThemeContext'
-import { headerData } from '../../data/headerData'
+// import { headerData } from '../../data/headerData'
 
 function Footer() {
 
@@ -17,13 +17,13 @@ function Footer() {
 
     return (
         <div className="footer" style={{backgroundColor: theme.secondary}}>
-            <p style={{color: theme.tertiary}}>
+            {/* <p style={{color: theme.tertiary}}>
                 Made with  
                 <span style={{color: theme.primary, margin: '0 0.5rem -1rem 0.5rem'}}>
                     ❤
                 </span>
                  by {shortname(headerData.name)}
-            </p>
+            </p> */}
         </div>
     )
 }
