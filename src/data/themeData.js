72d0ea52ec06e4fe100e72188ce84b17cb5705cf
thedanieljk <thedanieljk@gmail.com>
2022-08-11 08:38:15 -0700

    mobile changes

--- src/data/themeData.js
+++ src/data/themeData.js
@@ -1,12 +1,9 @@
 /* eslint-disable */
 import { 
-    greenThemeLight, greenThemeDark, bwThemeLight, bwThemeDark, blueThemeLight, blueThemeDark, redThemeLight, redThemeDark, orangeThemeLight, orangeThemeDark, purpleThemeLight, purpleThemeDark, pinkThemeLight, pinkThemeDark, yellowThemeLight, yellowThemeDark
+    blueThemeDark
 } from '../theme/theme'
 
 
 export const themeData = {
     theme: blueThemeDark
 }
-
-
-// Choose theme from above
