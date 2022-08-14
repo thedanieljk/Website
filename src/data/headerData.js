a1505892b9c01d4ca65273470a9219b482a61a2f
thedanieljk <thedanieljk@gmail.com>
2022-08-14 10:24:49 -0700

    center landing and upload new image

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Daniel Kirkpatrick',
     title: "",
     desciption:"I'm a software engineer born and raised in the Bay Area. I'm currently seeking a remote position. I'm very passionate about technology and love understanding how things work. Outside of tech, I spend my time outdoors, surfing and hiking at every opportunity.",
-    image: 'https://raw.githubusercontent.com/thedanieljk/Website/e20470f1ae79c188189c4bd242b0fe12fa69d2d2/src/assets/png/C6AE959A-01F0-4769-8FEB-E0A537D8C9D0.jpeg',
+    image: 'https://raw.githubusercontent.com/thedanieljk/Website/510c3bcdc6dbdad81707c544b695e37bc42f5605/src/assets/png/IMG_6665.jpg',
     resumePdf: resume
 }
