e51280a7a53810dbb1ecb8a09875747cb098cda4
thedanieljk <thedanieljk@gmail.com>
2022-08-18 13:51:17 -0700

    change console log

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ function App() {
 
   const { theme } = useContext(ThemeContext);
 
-  console.log("%cDEVELOPER PORTFOLIO", `color:${theme.primary}; font-size:50px`);
-  console.log("%chttps://github.com/hhhrrrttt222111/developer-portfolio", `color:${theme.tertiary}; font-size:20px`);
+  console.log("%cDANIEL'S PORTFOLIO", `color:${theme.primary}; font-size:50px`);
   // console.log = console.warn = console.error = () => {};
 
   return (
