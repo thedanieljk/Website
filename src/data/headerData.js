461f6df4231e1a8cc106ec984fa7a8ce5fc35e8a
thedanieljk <thedanieljk@gmail.com>
2022-08-18 13:58:46 -0700

    add to skills

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -3,7 +3,7 @@ import resume from '../assets/pdf/resume.pdf'
 export const headerData = {
     name: 'Daniel Kirkpatrick',
     title: "",
-    desciption:"I'm a software engineer born and raised in the Bay Area. So unique, right? I'm currently seeking a remote position. I'm very passionate about technology and love understanding how things work under the surface. Outside of tech, I spend my time outdoors, surfing and hiking at every opportunity.",
+    desciption:"I'm a software engineer born and raised in the Bay Area. So unique, right? I'm currently seeking a remote position. I'm very passionate about technology and love understanding how things work under the surface. Outside of tech, I spend my time outdoors, surfing and hiking at every opportunity. I also love spending time with my adorable cat, Luna.",
     image: 'https://raw.githubusercontent.com/thedanieljk/Website/510c3bcdc6dbdad81707c544b695e37bc42f5605/src/assets/png/IMG_6665.jpg',
     resumePdf: resume
 }
--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -2,9 +2,13 @@
     'Python',
     'Java',
     'AWS',
-    'Javascript'
-    // ,'Node JS'
+    'Javascript',
+    'Git',
+    'Node JS',
+    'React'
 ]
+
+//add git
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
