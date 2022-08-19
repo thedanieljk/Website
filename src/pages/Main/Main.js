02bb9b9804318c3b2272a840629d351aafe0874b
thedanieljk <thedanieljk@gmail.com>
2022-08-19 10:34:27 -0700

    remove about section and old assets

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-import { Navbar, Landing, About, Skills, Education, Experience, Contacts, Projects, Achievement } from '../../components'
+import { Navbar, Landing, Skills, Education, Experience, Contacts, Projects, Achievement } from '../../components'
 import { headerData } from '../../data/headerData'
 
 function Main() {
     return (
         <div>
             <Helmet>
-                <title>{headerData.name}'s' - Porfolio</title>
+                <title>{headerData.name}'s' - Portfolio</title>
             </Helmet>
 
             <Navbar />        
