ea0336125418c9cabf62c2982b41f4bbfc31c5ec
thedanieljk <thedanieljk@gmail.com>
2022-08-19 18:15:57 -0700

    achieveCard Changes

--- src/components/Achievement/AchievementCard.js
+++ src/components/Achievement/AchievementCard.js
@@ -28,17 +28,19 @@ function AchievementCard({id, title, details, date, field, image}) {
         <Fade bottom>
            <div key={id} className={`achievement-card ${classes.achievementCard}`}>
                <div className="achievecard-content">
-                    <div className="achievecard-details1">
-                        <h2 style={{color: theme.tertiary}}>{title}</h2>
-                        <p style={{color: theme.tertiary80}}>{details}</p>
-                    </div>
-                    <div className="achievecard-details2" style={{color: theme.primary}}>
-                        <h5>{date}</h5>
-                        <div className="achievecard-field">
-                            <AiOutlineFolder />
-                            <h5>{field}</h5>
-                        </div>   
-                    </div>
+                    <div className="achievecard-details-container">
+                        <div className="achievecard-details1">
+                            <h2 style={{color: theme.tertiary}}>{title}</h2>
+                            <p style={{color: theme.tertiary80}}>{details}</p>
+                         </div>
+                        <div className="achievecard-details2" style={{color: theme.primary}}>
+                            <h5>{date}</h5>
+                            <div className="achievecard-field">
+                                <AiOutlineFolder />
+                                <h5>{field}</h5>
+                            </div>   
+                        </div>
+                    </div>  
                 </div> 
                 <div className="achievecard-imgcontainer">
                     <img src={image} alt="" />
