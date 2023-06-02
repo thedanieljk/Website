263382cfc1f25e94f9311eca2a3a6f4f4216d179
Daniel Kirkpatrick <thedanieljk@gmail.com>
2023-06-02 15:13:19 -0700

    add SLO

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,7 +1,7 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'California Polytechnic State University',
+        institution: 'California Polytechnic State University - San Luis Obispo',
         course: 'Computer Science',
         startYear: '2014',
         endYear: '2020',
