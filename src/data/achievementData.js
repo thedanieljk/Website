4070a787b080ddc8fcc4692e453adce18ca57e08
Daniel Kirkpatrick <thedanieljk@gmail.com>
2023-06-02 14:48:32 -0700

    update achievement data (dates and details)

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -5,15 +5,15 @@ export const achievementData = {
             id : 1,
             title : 'AWS Solutions Architect - Associate [SAA-C03]',
             details : 'Validates technical expertise in designing and deploying scalable, highly available, and fault-tolerant systems on Amazon Web Services.',
-            date : 'September, 2022',
+            date : 'March 2023',
             field : 'Cloud',
             image : 'https://raw.githubusercontent.com/thedanieljk/Website/ba529b0a4c58b88d50abfb157ac506b13ad31642/src/assets/png/AWS-Certified-Solutions-Architect-Associate_badge.3419559c682629072f1eb968d59dea0741772c0f.png'
         },
         {
             id : 2,
-            title : 'COMPTIA Security+',
-            details : 'Validates cybersecurity knowledge',
-            date : 'September, 2022',
+            title : 'CompTIA Security+ [SY0-601]',
+            details : 'CompTIA Security+ is a global certification that validates the baseline skills necessary to perform core security functions and pursue an IT security career.',
+            date : 'February 2023',
             field : 'Cybersecurity',
             image : 'https://raw.githubusercontent.com/thedanieljk/Website/ba529b0a4c58b88d50abfb157ac506b13ad31642/src/assets/png/AWS-Certified-Solutions-Architect-Associate_badge.3419559c682629072f1eb968d59dea0741772c0f.png'
         },
