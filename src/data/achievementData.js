977c9d7c370311090e7818739d6b5af0903d3405
Daniel Kirkpatrick <thedanieljk@gmail.com>
2023-06-09 15:22:54 -0700

    cert descriptions

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -4,7 +4,7 @@ export const achievementData = {
         {
             id : 1,
             title : 'AWS Solutions Architect - Associate [SAA-C03]',
-            details : 'Validates technical expertise in designing and deploying scalable, highly available, and fault-tolerant systems on Amazon Web Services. Shows knowledge and skills in compute, networking, storage, and database AWS services as well as AWS deployment and management services. Includes experience in deploying, managing, and operating workloads on AWS as well as implementing security controls and compliance requirements',
+            details : 'Validates technical expertise in designing and deploying scalable, highly available, and fault-tolerant systems on Amazon Web Services. Shows knowledge and skills in compute, networking, storage, and database AWS services. Includes experience in deploying, managing, and operating workloads on AWS as well as implementing security controls and compliance requirements.',
             date : 'March 2023',
             field : 'Cloud',
             image : 'https://raw.githubusercontent.com/thedanieljk/Website/main/src/assets/png/awsSAAimg.png'
@@ -12,7 +12,7 @@ export const achievementData = {
         {
             id : 2,
             title : 'CompTIA Security+ [SY0-601]',
-            details : 'CompTIA Security+ is a global certification that validates the baseline skills necessary to perform core security functions and pursue an IT security career.',
+            details : 'CompTIA Security+ is a global certification that validates the baseline skills necessary to perform core security functions and pursue an IT security career. Covers analyzing threats or vulnerabilities, regulation compliance, and designing and evaluating security systems.',
             date : 'February 2023',
             field : 'Cybersecurity',
             image : 'https://raw.githubusercontent.com/thedanieljk/Website/9437ec8b2251cf1103d1dce4c811ae61cab9ebc5/src/assets/png/CompTIA_Security_2Bce.png'
