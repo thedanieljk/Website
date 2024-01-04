bf4b80add4cfde4fc5cb614df1e3d2474c8ac27e
Daniel Kirkpatrick <thedanieljk@gmail.com>
2024-01-04 14:19:59 -0800

    update header photo

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Daniel Kirkpatrick',
     title: "",
     desciption:"I'm a software engineer who is currently seeking a remote position. My main technical interests are in Cybersecurity, AR/VR, AI, and web development. I'm very passionate about technology and love understanding how things work under the surface. Outside of tech, I often spend my time outdoors, surfing and hiking at every opportunity. I also love playing video games and spending time with my adorable cat, Luna.",
-    image: 'https://raw.githubusercontent.com/thedanieljk/Website/510c3bcdc6dbdad81707c544b695e37bc42f5605/src/assets/png/IMG_6665.jpg',
+    image: 'https://github.com/thedanieljk/Website/blob/3a046e52f99bfeb9b1c1cb04f1640fac373a419a/src/assets/png/headshot.jpg',
     resumePdf: resume
 }
